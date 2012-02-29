cccb0c57b971ec4bbaa1b9245dbd1c8810218e87
jimmytidey <jimmytidey@gmail.com>
2012-02-29 22:55:14 +0000

    first draft

--- html/index.php
+++ html/index.php
@@ -11,19 +11,39 @@
 			<img src='resources/title.png' />
 			
 			<div id='explain'>
-				<p>Making music physical</p>
+				<h3>Reconnecting music to physical objects</h3>
 				
-				<p>Music has always been affected by technology: Beethoven's composition was influenced by the increasingly large pianos that were available to him, the 3 minute pop song is a consequence of the playback time of 78rpm vinyl.   
-
-				<p>Nearly everyone listens to music on a device with a powerful computer in it - whether it's a laptop or an iPod nano, but the only thing we use it for is o compress music into the MP3 format.</p>
-
-				<p>Riff lets composers take advantage of the algorithmic power of computer playback.</p> 
+				<p>Someone's vinyl collection says a lot about them, but who could say they really care about every song on their iPod?</p>
 				
+				<p>The convenience of MP3s is the reason for their success, but it also reduces the bond between people and their music. Rifff is the result of thinking about formats that engage users with their music collections by making them physical.</p>
+				
+				<p></p>
+				
+				<h3>What is it?</h3>
+				
+				<p>Rifff is a format that allows the composition of music that is different each time it is played, just like a live performance. Our focus is on our cloud-based composition tool and the physical player.</p>
+				
+				<p class='caption'><em>Composition tool</em></p>
 				<img src='resources/composer.png' />
 				
-				<h2><a href='http://jimmytidey.co.uk' target='_blank' >Jimmy Tidey</a></h2>
-				<h2><a href='http://thomasjbryan.co.uk' target='_blank' >Tom Bryan</a></h2>
-								
+				<br /><br />
+				
+				<p class='caption'><em>Experimental embeddable web player</em></p>	
+				<img src='resources/player.png' />
+
+				<h3>How does this reconnect music with physical objects?</h3>
+				
+				<p>Music has always been affected by technology - the 3 minute pop song is a result of the length of 78rpm records. Now, we play music on devices capable of sophisticated computation, but we don't take advantage of it.</p>  
+				
+				<p>Because the Rifff format requires some computation it cannot be shared via YouTube or other similar services. As a result, musicians can release music on a physical device and be sure that the music will always be deeply linked to that device - it cannot be copied up to the web.</p>				
+				
+				<p>We are currently developing a physical Rifff player on Arduino, the prototype version will cost about £50. We believe this can be greatly reduced.</p> 
+
+				
+				<h3>Who is making Rifff?</h3>
+				<p>Digital producer / developer <a href='http://jimmytidey.co.uk' target='_blank' >Jimmy Tidey</a> &amp;
+				<a href='http://thomasjbryan.co.uk' target='_blank' >Tom Bryan</a>, musician.</p>
+				<br / <br />		
 			</div>
 		
 		</div>
