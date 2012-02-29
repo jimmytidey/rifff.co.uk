75ea589336e4063e13bf7bc71247bf7bdca59ac9
jimmytidey <jimmytidey@gmail.com>
2012-02-29 12:04:59 +0000

    add

---
+++ html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title>RIFFF</title>
+		<script type="text/javascript" charset="utf-8" src=''></script>
+		<link rel="stylesheet" href="css/main.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	</head>
+	<body id="index">
+		<div id='content'>
+			<img src='resources/title.png' />
+			
+			<div id='explain'>
+				<p>Making music physical</p>
+				
+				<p>Music has always been affected by technology: Beethoven's composition was influenced by the increasingly large pianos that were available to him, the 3 minute pop song is a consequence of the playback time of 78rpm vinyl.   
+
+				<p>Nearly everyone listens to music on a device with a powerful computer in it - whether it's a laptop or an iPod nano, but the only thing we use it for is o compress music into the MP3 format.</p>
+
+				<p>Riff lets composers take advantage of the algorithmic power of computer playback.</p> 
+				
+				<img src='resources/composer.png' />
+				
+				<h2><a href='http://jimmytidey.co.uk' target='_blank' >Jimmy Tidey</a></h2>
+				<h2><a href='http://thomasjbryan.co.uk' target='_blank' >Tom Bryan</a></h2>
+								
+			</div>
+		
+		</div>
+		
+		
+		
+	</body>
+</html>
