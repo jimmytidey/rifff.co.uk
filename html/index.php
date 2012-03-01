ff26e170aed6d894351f15903d14026989b7adf6
jimmytidey <jimmytidey@gmail.com>
2012-03-01 09:28:10 +0000

    copy

--- html/index.php
+++ html/index.php
@@ -13,14 +13,14 @@
 			<div id='explain'>
 				<h3>Reconnecting music to physical objects</h3>
 				
-				<p>Someone's vinyl collection says a lot about them, but who could say they really care about every song on their iPod?</p>
+				<p>Someone's CD collection is deeply personal, but who could say they really care about every song on their iPod?</p>
 				
-				<p>Convenience explains the ubiquity of MP3s, but convenience also diminishes the bond between people and their music. Rifff is the result of thinking about formats that engage us with music by making desirable objects an intrinsic part of the experience.</p>
+				<p>Convenience explains the ubiquity of MP3s, but convenience also diminishes the bond between us and the music we listen to. Rifff is the result of thinking about formats that engage fans by making desirable objects an intrinsic part of the listening experience.</p>
 				
 				
 				<h3>What is Rifff?</h3>
 				
-				<p>Rifff is a format that allows the composition of music that is different each time it is played, just like a live performance. We are developing a cloud-based composition tool and a physical player.</p>
+				<p>Rifff is a format that allows the composition of music that is (subtly) different every time it is played, just like a live performance. We are developing a cloud-based composition tool and a physical player.</p>
 				
 				<p class='caption'><em>Composition tool</em></p>
 				<img src='resources/composer.png' />
@@ -32,9 +32,9 @@
 
 				<h3>How does this reconnect music with physical objects?</h3>
 				
-				<p>Music has always been affected by technology - the 3 minute pop song is a result of the length of 78rpm records. Now, we play music on devices capable of sophisticated computation but we don't take advantage of it.</p>  
+				<p>Music has always been affected by technology - the 3 minute pop song is a result of the length of 78rpm records. Now, we play music on devices capable of sophisticated computation but treat the devices as though they CD players with more storage.</p>  
 				
-				<p>Rifff uses the power of playback devices to the full, and as a result Riffs cannot be shared via YouTube or similar. Musicians can release music on a physical device and be sure that the music will always be deeply linked to that device - it cannot be copied up to the web.</p>				
+				<p>Rifff takes advantage of ubiquitous and cheap computational power on playback devices. This specialisation means Riffs cannot be shared via YouTube or similar. Musicians can release music on a physical device and be sure that the music will always be deeply linked to that device - it cannot be copied up to the web.</p>				
 				
 				<p>We are currently developing a physical Rifff player on Arduino, the prototype version will cost about £50. We believe this can be greatly reduced.</p> 
 
