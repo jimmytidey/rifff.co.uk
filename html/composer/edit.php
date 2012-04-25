05819d1035c40060977ebb7f2b627adb590bedc7
jimmytidey <jimmytidey@gmail.com>
2012-04-25 23:00:29 +0100

    automated commit

---
+++ html/composer/edit.php
@@ -0,0 +1,35 @@
+<? include('header.php') ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+	<link rel='stylesheet' href="style/style.css" >
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+
+    
+</head>
+<body>
+    
+<div id='edit'> 
+    
+    <h1 id='title'><a href='../composer/'>&laquo; Back</a>&nbsp; &nbsp;<? echo $_GET['project_name'] ?></h1>
+    <h1 id='files'><a href="upload/index.php?project_name=<? echo $_GET['project_name'] ?>" target='_blank'>Sample Upload</a></h1> 
+  
+    <div id="flash"> 
+    	<object type="application/x-shockwave-flash"  data="flash/random_seed_composer.swf?project_info_location=list.php?project_name=<? echo rawurlencode($_GET['project_name']) ?>"  width="960" height="2600">
+    	</object>
+    </div>
+    
+
+    
+</div>    
+    
+
+
+<? include('footer.php') ?>
+
+
