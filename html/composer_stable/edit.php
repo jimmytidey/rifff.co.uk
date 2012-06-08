bb9b6d655c27efc660b3f448eac73ba76e6dec87
Jimmy Tidey <jimmytidey@gmail.com>
2012-06-08 08:45:40 -0700

    hiding the front page

---
+++ html/composer_stable/edit.php
@@ -0,0 +1,76 @@
+<? include('header.php') ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+	<link rel='stylesheet' href="style/style.css" >
+
+
+    
+</head>
+<body>
+    
+<div id='edit'> 
+    
+    <h1 id='title'><a href='../composer/'>&laquo; Back</a>&nbsp; &nbsp;<? echo $_GET['project_name'] ?></h1>
+    <h1 id='files'><a href="upload/index.php?project_name=<? echo $_GET['project_name'] ?>" target='_blank'>Sample Upload</a></h1> 
+  
+    <br/> <br/><br/> <br/>
+  	<p><a href='#help' class='help_link' > Click here for help</a></p>
+
+    <div id="flash"> 
+    	<object type="application/x-shockwave-flash"  data="flash/random_seed_composer.swf?debug=false&project_name=<? echo rawurlencode($_GET['project_name']) ?>&username=<? echo $_SESSION['user_id'] ?>"  width="960" height="2600">
+    	</object>
+    </div>
+    
+    <div class='help'> 
+        <h1><a name='help'>Help</a></h1>
+
+        <strong>Rifff will not play back properly until all samples have uploaded fully, as indicated by the percentages at the bottom of the screen.</strong>
+
+        <h2>What is this?</h2> 
+        <p>Rifff is a composition tool. It is for composing music with an element of random.</p>
+
+        <p>It functions exactly like an audio sequencer, with each step of the sequence triggering a sample.</p> 
+
+        <p>However, as well as specifying that a sample should play or should not play, you can specify a 33% or 66% likelihood.</p>
+
+        <h2>How does it work</h2> 
+        <p>At the top of the screen are the settings for your composition: 
+        
+        <ul> 
+            <li><em>BPL</em> beats per loop - bar length</li>
+            <li><em>BPM</em> Beats per minute</li>
+            <li><em>Number of steps</em> the total length of the composition.</li> 
+        </ul>
+
+        <p>There are buttons to play, step forward, step backwards and rewind to the beginning. There is also a randomise button. Every time you press this button a new set of samples will be chosen from the options you have selected on the grid.</p> 
+
+        <p>The samples are divided into <em>banks</em>. Only one sample from each bank will play at a time.</p> 
+
+        <p>Against each bank there are two settings and a volume slider. The first setting <em>Loop</em> only takes affect if the mp3 file uploaded is shorter than a bar in your compisition. If this setting is selected the sample will loop unitl the end of the bar. The overplay setting only takes affect if your uploaded mp3 is longer than a bar. If it is selected it will cause the sample to play to the end, rather than cutting it off at the end of the bar.</p> 
+
+        <p>SEEMLESS LOOPING between steps of the composition can also be achieved by selecting two steps in a row, and selecting the "loop" button.</p>
+
+        <h2>Uploading</h2> 
+        <p>On the top right is the uploaded link, you can upload multiple files at a time. </p> 
+
+        <p>Once samples have uploaded, you need to press the Reload Samples button at the bottom of the composition screen.</p>
+        <p>Uploading samples with the same name will over-write pervious samples.</p>
+        <p>To select a sample for use in your composition, just use the drop downs next to the volume slider.</p>
+
+    </div>
+    
+</div>    
+    
+
+
+<? include('footer.php') ?>
+
+
---
+++ html/index_bak
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title>RIFFF</title>
+		<script type="text/javascript" charset="utf-8" src=''></script>
+		<link rel="stylesheet" href="css/main.css" type="text/css" media="screen" title="no title" charset="utf-8">
+		<link rel="icon" type="image/png"  href="favicon.ico">
+	</head>
+	<body id="index">
+		<div id='content'>
+			<img src='resources/title.png' />
+			
+			<div id='explain'>
+				<h3>Reconnecting music to physical objects</h3>
+				
+				<p>Someone's CD collection is deeply personal, but who could say they really care about every song on their iPod?</p>
+				
+				<p>Everyone listens to MP3s because they are convenient, but convenience can also diminish the bond between us and the music we listen to. Rifff is the result of thinking about formats that engage fans by making desirable objects an intrinsic part of the listening experience.</p>
+				
+				<h3>What is Rifff?</h3>
+				
+				<p>Rifff is a format that allows the composition of music that is (subtly) different every time it is played, just like a live performance. We are developing a cloud-based composition tool and a physical player.</p>
+				
+				<p class='caption'><em>Composition tool</em></p>
+				<img src='resources/composer.png' />
+				
+				<br /><br />
+				
+				<p class='caption'><em>Experimental embeddable web player</em></p>	
+				<img src='resources/player.png' />
+
+				<h3>How does this reconnect music with physical objects?</h3>
+				
+				<p>Music has always been affected by technology - the 3 minute pop song is a result of the length of 78rpm records. Now, we play music on devices capable of sophisticated computation but treat the devices as though they CD players with more storage.</p>  
+				
+				<p>Rifff takes advantage of ubiquitous and cheap computational power on playback devices. This specialisation means Riffs cannot be shared via YouTube or similar. Musicians can release music on a physical device and be sure that the music will always be deeply linked to that device - it cannot be copied up to the web.</p>				
+				
+				<p>We are currently developing a physical Rifff player on Arduino, the prototype version will cost about £50. We believe this can be greatly reduced.</p> 
+                
+                <object type='application/x-shockwave-flash' data='http://riff.jimmytidey.co.uk/FLASH%20SRC/random_seed_player.swf' width='500' height='300'><param name='flashvars'  /><param name='allowScriptAccess' value='always' /><param name='movie' value='http://riff.jimmytidey.co.uk/FLASH%20SRC/random_seed_player.swf' /></object>
+				
+				<h3>Who is making Rifff?</h3>
+				<p>Digital producer / developer <a href='http://jimmytidey.co.uk' target='_blank' >Jimmy Tidey</a> &amp;
+				<a href='http://thomasjbryan.co.uk' target='_blank' >Tom Bryan</a>, musician.</p>
+				<br / <br />		
+			</div>
+		
+		</div>
+		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-29624850-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+		
+		
+		
+	</body>
+</html>
