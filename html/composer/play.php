bd9519fb32f0395ec9c792e56cb29dbc9a10be57
jimmytidey <jimmytidey@gmail.com>
2012-06-12 12:11:02 +0100

    adding a player

--- html/composer/play.php
+++ html/composer/play.php
@@ -10,7 +10,7 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 
 	<link rel='stylesheet' href="style/style.css" >
-    <title> Edit -user: <?= $user_id ?></title>
+    <title> Play -user: <?= $user_id ?></title>
 
     
 </head>
@@ -19,14 +19,10 @@
 <div id='edit'> 
     
     <h1 id='title'><a href='../composer/'>&laquo; Back</a>&nbsp; &nbsp;<? echo $_GET['project_name'] ?></h1>
-    <h1 id='files'><a href="upload/index.php?project_name=<? echo $_GET['project_name'] ?>" target='_blank'>Sample Upload</a></h1> 
-  
-    <br/> <br/><br/> <br/>
-  	<p><a href='#help' class='help_link' > Click here for help</a></p>
-    <p><a href='play.php?project_name=<?= $_GET['project_name'] ?>'>  class='help_link' > Click here to view the player</a></p>
+
 
     <div id="flash"> 
-    	<object type="application/x-shockwave-flash"  data="flash/random_seed_composer.swf?debug=false&project_name=<? echo rawurlencode($_GET['project_name']) ?>&username=<? echo $_SESSION['user_id'] ?>"  width="960" height="2600">
+    	<object type="application/x-shockwave-flash"  data="flash/random_seed_player.swf?debug=false&project_name=<? echo rawurlencode($_GET['project_name']) ?>&username=<? echo $_SESSION['user_id'] ?>"  width="960" height="2600">
     	</object>
     </div>
     
