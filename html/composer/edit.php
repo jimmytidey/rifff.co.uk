793a00cb37277d4638c08a1424d43cf93661b2ea
jimmytidey <jimmytidey@gmail.com>
2012-06-14 15:21:49 +0100

    mega refactor

--- html/composer/edit.php
+++ html/composer/edit.php
@@ -30,43 +30,7 @@
     	</object>
     </div>
     
-    <div class='help'> 
-        <h1><a name='help'>Help</a></h1>
-
-        <strong>Rifff will not play back properly until all samples have uploaded fully, as indicated by the percentages at the bottom of the screen.</strong>
-
-        <h2>What is this?</h2> 
-        <p>Rifff is a composition tool. It is for composing music with an element of random.</p>
-
-        <p>It functions exactly like an audio sequencer, with each step of the sequence triggering a sample.</p> 
-
-        <p>However, as well as specifying that a sample should play or should not play, you can specify a 33% or 66% likelihood.</p>
-
-        <h2>How does it work</h2> 
-        <p>At the top of the screen are the settings for your composition: 
-        
-        <ul> 
-            <li><em>BPL</em> beats per loop - bar length</li>
-            <li><em>BPM</em> Beats per minute</li>
-            <li><em>Number of steps</em> the total length of the composition.</li> 
-        </ul>
-
-        <p>There are buttons to play, step forward, step backwards and rewind to the beginning. There is also a randomise button. Every time you press this button a new set of samples will be chosen from the options you have selected on the grid.</p> 
-
-        <p>The samples are divided into <em>banks</em>. Only one sample from each bank will play at a time.</p> 
-
-        <p>Against each bank there are two settings and a volume slider. The first setting <em>Loop</em> only takes affect if the mp3 file uploaded is shorter than a bar in your compisition. If this setting is selected the sample will loop unitl the end of the bar. The overplay setting only takes affect if your uploaded mp3 is longer than a bar. If it is selected it will cause the sample to play to the end, rather than cutting it off at the end of the bar.</p> 
-
-        <p>SEEMLESS LOOPING between steps of the composition can also be achieved by selecting two steps in a row, and selecting the "loop" button.</p>
-
-        <h2>Uploading</h2> 
-        <p>On the top right is the uploaded link, you can upload multiple files at a time. </p> 
-
-        <p>Once samples have uploaded, you need to press the Reload Samples button at the bottom of the composition screen.</p>
-        <p>Uploading samples with the same name will over-write pervious samples.</p>
-        <p>To select a sample for use in your composition, just use the drop downs next to the volume slider.</p>
-
-    </div>
+ 
     
 </div>    
     
