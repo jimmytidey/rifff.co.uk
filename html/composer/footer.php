bf47dc3da81a1afc2ebf272695f811f60e016a28
jimmytidey <jimmytidey@gmail.com>
2012-07-07 21:46:31 +0100

    automated commit

--- html/composer/footer.php
+++ html/composer/footer.php
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29624850-1']);
+  _gaq.push(['_setAccount', 'UA-33230735-1']);
   _gaq.push(['_trackPageview']);
 
   (function() {
