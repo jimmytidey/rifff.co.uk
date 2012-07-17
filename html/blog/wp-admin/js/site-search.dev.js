4f4288784a7be24abbe60080692af6e9052b52f3
Jimmy Tidey <jimmytidey@gmail.com>
2012-07-17 06:13:07 -0700

    updating crossdomain.xml

--- html/blog/wp-admin/js/site-search.dev.js
+++
@@ -1,7 +0,0 @@
-jQuery( function($) {
-	$( '#site-search-input' ).autocomplete({
-		source:   ajaxurl + '?action=autocomplete-site',
-		delay:    500,
-		minLength: 2
-	});
-});
--- html/blog/wp-admin/js/site-search.js
+++
@@ -1,1 +0,0 @@
-jQuery(function(a){a("#site-search-input").autocomplete({source:ajaxurl+"?action=autocomplete-site",delay:500,minLength:2})});
